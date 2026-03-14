57f4ac98695b38fb28f80a247c311e69e3aba949
bryhmo <codewithbryhmo@gmail.com>
2026-03-14 15:00:38 +0100

    fix strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,11 +5,17 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor(private readonly configService: ConfigService) {
+    const secret = configService.get<string>('JWT_SECRET');
+
+    if (!secret) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      secretOrKey: secret,
     });
   }
 
